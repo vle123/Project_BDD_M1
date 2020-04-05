43a84bad64f6a2bbd87a5ca276a18383105ab9c9
Armand <armand.rebut@gmail.com>
2020-04-05 17:21:09 +0200

    mise a jour
    Co-Authored-By: deathpioupiou <deathpioupiou@users.noreply.github.com>

---
+++ public/javascripts/jsMore.js
@@ -0,0 +1,26 @@
+
+    obj = document.getElementsByClassName('resultat');
+    html = document.getElementsByTagName('html')[0];
+    nav = document.getElementsByTagName('nav')[0];
+    objet = document.getElementsByClassName('objet');
+    console.log(obj[0].offsetHeight+" "+obj[0].offsetWidth);
+
+    var w = obj[0].offsetWidth;
+    var h = html.offsetHeight-nav.offsetHeight;
+    var W = parseInt(w/92);
+    var H = parseInt(h/92)-1;
+    var nb = H*W;
+    console.log(H+" "+W);
+    compt = nb-objet.length;
+    console.log(compt);
+    for (let i = 0; i < compt; i++) {
+        var node = document.createElement("span");
+        node.className = "objet";
+        var n = objet[getRandomInt(objet.length)];
+        document.getElementsByClassName('resultat')[0].insertBefore(node,n);
+        
+    }
+    
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
